775afe4f1223e9b63f76502bb476f349b97e88b1
Salvatopre Scala <58303920+knowsx2@users.noreply.github.com>
2021-11-16 13:02:22 +0100

    sperimental analysis

--- test_generator.py
+++ test_generator.py
@@ -9,27 +9,32 @@ def remove_sublists(lists):
         if set(list1) <= set(list2):
             if list1 in lists:
                 lists.remove(list1)
-    return lists
+    return
 
 
 def generate():
-    n_solutions = randrange(10) + 2
-    n_players = randrange(11) + 3
+    n_solutions = randrange(5) + 3
+    n_players = randrange(5) + 3
     domains = []
     players = []
     solutions = []
-
+    domain = []
+    for _ in range(3):
+        domain.append(choice([i for i in range(1, n_players * 3 * 2 * n_solutions) if i not in domain]))
+    for _ in range(n_players):
+        domains.append(domain)
+    '''
     for _ in range(n_players):
         domain = []
-        n_domains = randrange(11) + 3
+        n_domains = 3
         for _ in range(n_domains):
             domain.append(choice([i for i in range(1, n_players * n_domains * 2 * n_solutions) if i not in domain]))
         domain = sorted(domain)
         domains.append(domain)
-
+    
     for i in range(n_players):
         players.append(Agent("a" + str(i), domains[i], domains[i][0]))
-
+    
     while len(solutions) < 2:
         for _ in range(n_solutions):
             solution = []
@@ -52,6 +57,17 @@ def generate():
         if not players:
             for i in range(n_players):
                 players.append(Agent("a" + str(i), domains[i], domains[i][0]))
+    '''
+    while len(solutions) < 2 or len(players) < 3:
+        solutions = []
+        players = []
+        for i in range(n_players):
+            players.append(Agent("a" + str(i), domains[i], domains[i][0]))
+        for _ in range(n_solutions):
+            solution = sample(players, randrange(1, n_players))
+            solutions.append(solution)
+        remove_sublists(solutions)
+        solutions, players = filter_solutions({players[i]: players[i].domain for i in range(n_players)}, solutions)
 
     directions = [randrange(2) for _ in range(len(players))]
     return players, directions, solutions
